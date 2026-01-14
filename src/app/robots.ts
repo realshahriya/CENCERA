1ed7e076c9036668390029d15e4007663dd65397
realshahriya <realshahriya@gmail.com>
2026-01-14 18:57:32 +0600

    test

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
     return {
+        
         rules: {
             userAgent: '*',
             allow: '/',
